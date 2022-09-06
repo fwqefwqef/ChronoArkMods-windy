c26c629e6180ecaa2e6e950775fe89de82e88211
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2022-09-06 14:17:00 +0900

    Add files via upload

--- (WIP) Expert+ Mod/Source/ExpertPlusMod.cs
+++ (WIP) Expert+ Mod/Source/ExpertPlusMod.cs
@@ -37,7 +37,7 @@ namespace ExpertPlusMod
             PermaMode = Config.Bind("Generation config", "Permadeath Mode", false, "Permadeath Mode\nCampfires cannot revive allies. Removed revive option in Medical Tent. Golden Bread cannot be used on fallen allies.");
             VanillaCurses = Config.Bind("Generation config", "Vanilla Curses", false, "Vanilla Curses\nReverts the nerfs to Cursed Mob stats. The challenge is designed around weaker cursed mobs, but toggle this on if you want.");
             AscensionMode = Config.Bind("Generation config", "Ascension Mode", false, "Ascension Mode\nA mimic of Slay The Spire's Ascension Mode.\n1. Add Slow Response Curse to deck at the start of the game.\n2. Maximum potion uses per battle reduced to 2.\n3. Character Equipment Slots reduced to 1. (Equipment Drop Rates reduced)\n4. Relic Slots reduced to 2.");
-            DespairMode = Config.Bind("Generation config", "Despair Mode", false, "Despair Mode\nWarning: Very Difficult.\n1. Lifting Scrolls do not spawn in battle.\n2. After Misty Garden 1, fight all possible bosses for each stage. Godo and TFK fight is harder.\n3. More features coming soon.");
+            DespairMode = Config.Bind("Generation config", "Despair Mode", false, "Despair Mode\nWarning: Very Difficult.\n1. Lifting Scrolls do not spawn in battle.\n2. After Misty Garden 1, fight all possible bosses for each stage. Godo and TFK fight is harder.");
             hardTransitions = Config.Bind("Generation config", "Hard Transitions", false, "Removes most of the boss transition handicap. Despair Mode needs to be on.");
 
             harmony.PatchAll();

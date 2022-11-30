7330e5288a2037275abf105e4fa15cc32abebc1b
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2022-11-30 17:47:46 +0900

    Add files via upload

---
+++ Fox Orb Genderlock Removal/FoxOrbGenderLockRemoval/FoxOrbGenderLockRemoval/Class1.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using GameDataEditor;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using I2.Loc;
+
+namespace LegendaryItemsPatch
+{
+    [BepInPlugin(GUID, "Fox Orb Genderlock Patch", version)]
+    [BepInProcess("ChronoArk.exe")]
+    public class LegendadryItemsPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "org.windy.chronoark.itemmod.foxorbgenderlock";
+        public const string version = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(GUID);
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+        void OnDestroy()
+        {
+            if (harmony != null)
+                harmony.UnpatchAll(GUID);
+        }
+
+        // Everyone is female now, seethe and cope
+        [HarmonyPatch(typeof(Misc), "IsFemale")]
+        class Foxorb_GenderLock_Patch
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                __result = true;
+                return false;
+            }
+        }
+
+    }
+}
